6f17f4eda6105d64cdc817debc9e22653ace6de8
Lok Lok <yellowhappy831@gmail.com>
2026-08-09 23:54:18 +0800

    save

--- src/web/client/App.tsx
+++ src/web/client/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import { BlenderReceiver } from "./pages/BlenderReceiver";
 import { Projects } from "./pages/Projects";
+import { ProjectEditor } from "./pages/ProjectEditor";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<Welcome />} />
       <Route path="/preview" element={<BlenderReceiver />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:projectID" element={<ProjectEditor />} />
     </Routes>
   );
 }
