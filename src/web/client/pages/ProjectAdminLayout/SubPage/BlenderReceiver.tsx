d25a4dfda75af7a18e7e21e4f778c8e927929641
Lok Lok <yellowhappy831@gmail.com>
2026-08-10 11:33:50 +0800

    save

--- src/web/client/pages/ProjectAdminLayout/SubPage/BlenderReceiver.tsx
+++ src/web/client/pages/ProjectAdminLayout/SubPage/BlenderReceiver.tsx
@@ -19,9 +19,14 @@ export function BlenderReceiver() {
         </CanvasGPU>
       </div>
       <div className=" absolute top-0 left-0">
-        {/*  */}
-        {/*  */}
-        {/*  */}
+        <div className="">
+          <button className="block px-3 py-1 text-white text-sm bg-blue-500 rounded-lg m-1">
+            Import to 3D Envrionment
+          </button>
+          <button className="block px-3 py-1 text-white text-sm bg-blue-500 rounded-lg m-1">
+            Import to 3D Props
+          </button>
+        </div>
       </div>
     </div>
   );
